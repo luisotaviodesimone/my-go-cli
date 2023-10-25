75dac2dff72a96cae962fdc204b83fc2c91824f8
luisotaviodesimone <luisotaviodesimone@gmail.com>
2023-10-24 22:21:14 -0300

    feat: add single run and base dir flags to clear command

--- internal/cmds/clearNodeModules.go
+++ internal/cmds/clearNodeModules.go
@@ -20,40 +20,60 @@ func removeNodeModules(parentFolder string, nodeModulesFullPath string) {
 	fmt.Printf("\n%s%s%s\n\n", colors.Green, "Done!", colors.Reset)
 }
 
-func clearNodeModules(baseDir string) {
+func clearNodeModules(baseFolder string, shouldNotRecurse bool) {
 
 	const (
 		folderToClear = "node_modules"
 	)
 
-	fileInfo, _ := os.Stat(baseDir)
+	fileInfo, _ := os.Stat(baseFolder)
 
 	if !fileInfo.IsDir() {
 		return
 	}
 
-	curDirFiles, _ := os.ReadDir(baseDir)
+	curDirFiles, _ := os.ReadDir(baseFolder)
 
 	for _, element := range curDirFiles {
-		elementFullPath := fmt.Sprintf("%s\\%s", baseDir, element.Name())
+		elementFullPath := fmt.Sprintf("%s/%s", baseFolder, element.Name())
 
 		if element.Name() == folderToClear && element.IsDir() {
-			removeNodeModules(baseDir, elementFullPath)
-		} else if element.IsDir() {
-			clearNodeModules(elementFullPath)
+			removeNodeModules(baseFolder, elementFullPath)
+		} else if element.IsDir() && !shouldNotRecurse {
+			clearNodeModules(elementFullPath, shouldNotRecurse)
 		}
 	}
 
 }
 
 func ClearNodeModulesCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "clear",
-		Short: "Clears `node_modules` folder recursively from the current directory",
+		Short: "Clears `node_modules` folder from the current directory and its subdirectories",
 		Args:  cobra.ExactArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			baseDir, _ := os.Getwd()
-			clearNodeModules(baseDir)
+
+			var baseFolder string
+			var err error
+
+			baseFolder = cmd.Flag("base-folder").Value.String()
+			shouldNotRecurse, _ := cmd.Flags().GetBool("single-execution")
+
+			if baseFolder == "" {
+				baseFolder, err = os.Getwd()
+			}
+
+			if err != nil {
+				fmt.Println("Error getting current directory")
+				return
+			}
+
+			clearNodeModules(baseFolder, shouldNotRecurse)
 		},
 	}
+
+	cmd.PersistentFlags().StringP("base-folder", "f", "", "Folder to start the cleaning from")
+	cmd.PersistentFlags().BoolP("single-execution", "s", false, "If the cleaning should only be done in the current directory")
+
+	return cmd
 }
