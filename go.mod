defe422f333dda45a89d74051e1bd7f3f6f65c21
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-09-14 15:13:02 -0300

    feat: update clean branches command

--- go.mod
+++ go.mod
@@ -5,8 +5,6 @@ go 1.18
 require (
 	github.com/atotto/clipboard v0.1.4
 	github.com/cli/go-gh/v2 v2.0.1
-	github.com/go-git/go-billy/v5 v5.5.0
-	github.com/go-git/go-git v4.7.0+incompatible
 	github.com/go-git/go-git/v5 v5.12.0
 	github.com/hegedustibor/htgo-tts v0.0.0-20230402053941-cd8d1a158135
 	github.com/manifoldco/promptui v0.9.0
@@ -24,6 +22,7 @@ require (
 	github.com/ebitengine/purego v0.3.2 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
 	github.com/hajimehoshi/oto/v2 v2.4.0 // indirect
@@ -34,14 +33,11 @@ require (
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.2.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/src-d/gcfg v1.4.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
-	gopkg.in/src-d/go-billy.v4 v4.3.2 // indirect
-	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- internal/cmds/git/cleanStaleBranches.go
+++ internal/cmds/git/cleanStaleBranches.go
@@ -2,10 +2,13 @@ package lodsgit
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+
 	"github.com/luisotaviodesimone/my-go-cli/internal/constants"
 
 	"github.com/spf13/cobra"
@@ -47,40 +50,54 @@ func CleanStaleBranches() *cobra.Command {
 			repo, err := git.PlainOpen(path)
 			checkIfError(err)
 
+			sshKeyFilePath := fmt.Sprintf("%s/.ssh/%s", os.Getenv("HOME"), "id_rsa")
+			keyFileBytes, err := os.ReadFile(sshKeyFilePath)
+
+			if err != nil {
+				log.Fatal("Unable to open ssh key file: ", err)
+			}
+
+			publicKeys, err := ssh.NewPublicKeys("git", keyFileBytes, "")
+			if err != nil {
+				log.Fatalf("Failed to create public keys: %v", err)
+			}
+
+			err = repo.Fetch(&git.FetchOptions{
+				RemoteName: "origin",
+				Auth:       publicKeys,
+				Progress:   os.Stdout,
+				Prune:      true,
+			})
+			if err != nil && err != git.NoErrAlreadyUpToDate {
+				fmt.Println("Fetch failed: ", err)
+			}
+
 			ref, err := repo.Branches()
-			checkIfError(err)
 
 			staleBranches := make(map[plumbing.ReferenceName]plumbing.Hash)
 			logInfo("Stale branches: ")
 			err = ref.ForEach(func(r *plumbing.Reference) error {
-				fmt.Printf("local: %v\n", r.Name().Short())
-
 				config, err := repo.Config()
 				if err != nil {
 					return err
 				}
 
-				branchConfig, ok := config.Branches[r.Name().Short()]
-				if !ok || branchConfig.Remote == "" || branchConfig.Merge == "" {
-					logInfo("  No upstream configured")
-					return nil
-				}
+				branchConfig, _ := config.Branches[r.Name().Short()]
 
 				upstreamRefName := plumbing.NewRemoteReferenceName(branchConfig.Remote, branchConfig.Merge.Short())
 				_, err = repo.Reference(upstreamRefName, true)
 				if err != nil {
 					staleBranches[r.Name()] = r.Hash()
-					logFail("  Failed to find upstream reference: ", err.Error())
 					return nil
-				}
-
-				for name, hash := range staleBranches {
-					message := fmt.Sprintf("  %s: %s", name, hash)
-					logFail(message)
 				}
 
 				return nil
 			})
+
+			for name := range staleBranches {
+				message := fmt.Sprintf("  %s", name)
+				logInfo(message)
+			}
 
 			checkIfError(err)
 		},
