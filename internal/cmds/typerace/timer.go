4eddc09d96fceb931b7749f18531f573a768ca97
Luís Otávio De Simone <16899031-luisotaviodesimone@users.noreply.gitlab.com>
2025-02-10 23:44:38 -0300

    fix: red timer

--- internal/cmds/typerace/timer.go
+++ internal/cmds/typerace/timer.go
@@ -35,14 +35,14 @@ func startTimer(timeChan chan string, timerTime float64) {
 func promptUser(inputChan, timeChan chan string) {
 	input := ""
 	timer := "0.000"
-	fmt.Print("\r", colors.Blue+"Start typing: "+colors.Reset, input, "█ ", timer, "secs")
+	fmt.Print("\r", colors.Blue+"Start typing: "+colors.Reset, input, "█ ", colors.Red, timer, "secs", colors.Reset)
 	for {
 		// TODO: Find a solution to the problem where the cursor not being at the end of the typed text
 		select {
 		case input = <-inputChan:
-			fmt.Print("\r", colors.Blue+"Start typing: "+colors.Reset, input, "█ ", colors.Red+timer, "secs"+colors.Reset)
+			fmt.Print("\r", colors.Blue+"Start typing: "+colors.Reset, input, "█ ", colors.Red, timer, "secs", colors.Reset)
 		case timer = <-timeChan:
-			fmt.Print("\r", colors.Blue+"Start typing: "+colors.Reset, input, "█ ", colors.Red+timer+colors.Reset, "secs"+colors.Reset)
+			fmt.Print("\r", colors.Blue+"Start typing: "+colors.Reset, input, "█ ", colors.Red, timer, "secs", colors.Reset)
 		default:
 		}
 	}
